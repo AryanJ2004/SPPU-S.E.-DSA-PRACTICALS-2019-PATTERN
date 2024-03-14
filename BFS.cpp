85af148ed5526129296c0e77698e041d5e221ff5
Aryan N. Jain <aryanj1084@gmail.com>
2024-03-14 23:28:42 +0530

    Update BFS.cpp

--- BFS.cpp
+++ BFS.cpp
@@ -5,22 +5,15 @@ void BFS(int A[][6], int n) {
     cout << "Enter Starting Vertex:"<<endl;
     int vtx;
     cin>>vtx;
-    int a=vtx;
-    if(vtx<=0)
-        vtx=0;
-    else
-        vtx-=1;
     int q[20]{0};
-    int visited[6]{0}; 
+    int visited[7]{0}; 
     int f = 0, r = -1; 
     q[++r] = vtx;
     visited[vtx] = 1; 
     while (f <= r) {
         int u = q[f++]; 
-        if(a<=0)
+
         cout << u << " ";
-        else
-        cout << u+1 << " "; //+1 if a is 1 or greater
         for (int v = 0; v < n; v++) {
             if (A[u][v] == 1 && visited[v] == 0) {
                 q[++r] = v;
@@ -30,14 +23,13 @@ void BFS(int A[][6], int n) {
     }
     cout << endl;
 }
-
 int main() {
-    int A[6][6] = {{0,1,0,0,1,0},
+    int A[6][6] = {{0,1,1,1,1,0},
+                   {1,0,0,1,0,0},
+                   {1,0,0,1,1,1},
+                   {1,1,1,0,0,1},
                    {1,0,1,0,0,0},
-                   {0,1,0,1,0,0},
-                   {0,0,1,0,0,1},
-                   {1,0,0,0,0,1},
-                   {0,0,0,1,1,0}};
+                   {0,0,1,1,0,0}};
     BFS(A, 6);
 
     return 0;
