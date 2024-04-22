20805fa7c1efecba60733f0b72e0f52b69260e1b
Aryan N. Jain <aryanj1084@gmail.com>
2024-04-22 20:40:59 +0530

    Binary Search Tree.cpp

--- BST.cpp
+++ BST.cpp
@@ -1,195 +1,173 @@
-
-//======================================INCOMPLETE PROGRAM==============================
-
-#include <iostream>
+#include<iostream>
 using namespace std;
-struct node
-{
-    int key;
-    int data;
-    struct node *left,*right;
-    
-};
 
-class stack
+struct Node
 {
-    struct node *data[20];
-    int top;
-    public:
-    stack()
-    {
-        top=-1;
-    }
-    void push(struct node*);
-    struct node *pop();
-    int isEmpty();
-    
+    int data;
+    Node *left=NULL,*right=NULL;
 };
 
 class BST
 {
-    struct node *root;
-    struct node *findmin(struct node*);
-    struct node *findmax(struct node*);
-    void swapper(struct node*);
-    int height_of_tree(sturct node*);
-    struct node *createnode();
     public:
+    Node *root;
     BST()
     {
         root=NULL;
     }
-    void createTree();
-    void insertnode();
-    void non_rec_inorder();
-    int searchtree(int);
-    struct node *findmin1()
-    {
-        struct node *min=findmin(root);
-        return min;
-    }
-    struct node *findmax1()
-    {
-        struct node *max=findmax(root);
-        return max;
-    }
-    void swapper1()
-    {
-        swapper(root);
-    }
-    void deleteNode1(int key)
-    {
-        root=deletenode(root,key);
-    }
-    void height_of_tree1()
-    {
-        int height=height_of_tree(root);
-        return height;
-    }
+    void insert();
+    void display_inorder(Node *root);
+    int min();
+    int longest_path(Node *nd);
+    void search(int x);
+    void mirror(Node *root);
 };
 
-void BST::createTree()
+void BST::insert()
 {
     int n;
-    cout<<"Enter How Many Nodes You Want To Enter:?"<<endl;
+    cout<<"ENTER THE VALUE :";
     cin>>n;
-    for(int i=0;i<n;i++)
-    {
-        insertnode();
-    }
-}
-struct node* BST::createnode()
-{
-    struct node *newNode=new node;
-    cout<<"Enter Data"<<endl;
-    cin>>newNode->data;
-    newNode->left=newNode->right=NULL;
-    return (newNode);
-}
-
-void BST::insertnode()
-{
-    struct node *newNode=createnode();
+    
+    Node *temp=new Node;
+    temp->data=n;
+    
     if(root==NULL)
-    root=newNode;
-    else
     {
-        struct node *old;
-        struct node *temp=root;
-        while(temp!=NULL)
-        {
-            if(newNode->data<temp->data)
-            {
-                old=temp;
-                temp=temp->left;
-            }
-            else
-            {
-                old=temp;
-                temp=temp->right;
-            }
-            
-        }
-        if(newNode->data<old->data)
-        old->left=newNode;
-        else
-        old->right=newNode;
-    }
-}
-
-void BST::non_rec_inorder()
-{
-    stack stk;
-    struct node *temp=root;
-    if(temp==NULL)
-    {
-        cout<<"\nTree Is Empty"<<endl;
-    }
-    while(temp!=NULL)
-    {
-        stk.push(temp);
-        temp=temp->left;
-    }
-    while(!stk.isEmpty())
-    {
-        struct node *ele=stk.pop();
-        cout<<ele->data<<" ";
-        ele=ele->right;
-        while(ele!=NULL)
-        {
-            stk.push(ele);
-            ele=ele->left;
-        }
-    }
-}
-
-void stack::push(struct node *ele)
-{
-    top++;
-    data[top]=ele;
-}
-
-struct node *stack::pop()
-{
-    struct node* ele=data[top];
-    top--;
-    return ele;
-}
-
-int stack::isEmpty()
-{
-    if(top==-1)
-    return 1;
-    return 0;
-}
-
-int BST::searchtree(int key)
-{
-    struct node *temp=root;
-    if(temp==NULL)
-    {
-        cout<<"Tree Is Empty"<<endl;
-        
-    }
-    while(temp!=NULL)
-    {
-        if(temp->data==key)
-        return 1;
-        if(temp->data>key)
-        temp=temp->left;
-        else
-        temp=temp->right;
-    }
-    return 0;
+        root = temp;
+        return;
     }
     
-struct node* BST::findmin(struct node *temp)
-{
-    struct node *old;
-    while(temp!=NULL)
+    Node *trav=root;
+    
+    while(true)
     {
-        old=temp;
-        temp=temp->right;
+        if(trav->data>n)
+            if(trav->left==NULL)
+            {
+                trav->left=temp;
+                return;
+            }
+            else
+                trav=trav->left;
+        else
+            if(trav->right==NULL)
+            {
+                trav->right=temp;
+                return;
+            }
+            else
+                trav=trav->right;
     }
-    return old;
 }
+
+void BST::display_inorder(Node *root)
+{
+    if(root == NULL)   
+        return;
+    display_inorder(root->left);
+    cout<<root->data<<" ";
+    display_inorder(root->right);
+}
+
+int BST::min()
+{
+    if(root==NULL)
+        return -1;
+    Node *trav=root;
+    while(trav->left!=NULL)
+        trav=trav->left;
+    return trav->data;
+}
+
+int BST::longest_path(Node *nd)
+{
+    if(nd == NULL)
+        return 0;
+
+    int ll = longest_path(nd->left);
+    int lr = longest_path(nd->right);
+
+    return 1 + max(ll,lr);
+}
+
+void BST::search(int x)
+{
+    Node *trav=root;
+    while(trav!=NULL)
+    {
+        if(trav->data==x)
+        {
+            cout<<"DATA FOUND"<<endl;
+            return;
+        }
+        else if(trav->data>x)
+            trav=trav->left;
+        else
+            trav=trav->right;
+    }
+    cout<<"DATA NOT FOUND"<<endl;
+}
+
+void BST::mirror(Node *root)
+{
+    if(root==NULL)
+        return;
+    mirror(root->left);
+    mirror(root->right);
+    Node *temp=root->left;
+    root->left=root->right;
+    root->right=temp;
+}
+
+int main ()
+{
+
+    BST b;
+    int ch;
+    do
+    {
+        cout<<"\n=====================MENU=====================\n";
+        cout<<"1.INSERT NODE\n2.DISPLAY INORDER OF THE TREE\n3.FIND NUMBER OF NODES IN LONGEST PATH FROM ROOT\n4.MINIMUM DATA VALUE FOUND IN THE TREE\n5.MIRROR TREE\n6.SEARCH A NODE BY VALUE\n0.EXIT\n";
+        cout<<"\nENTER YOUR CHOICE :";
+        cin>>ch;
+        switch (ch)
+        {
+    	    case 1:
+        	    b.insert();
+        	    break;
+
+    	    case 2:
+    	        cout << "\nINORDER TRAVERSAL : ";
+                b.display_inorder (b.root);
+                cout << endl;
+                break;
+
+            case 3:
+                cout << "NUMBER OF NODES IN LONGEST PATH FROM ROOT : ";
+                cout << b.longest_path (b.root) << endl;
+                break;
+
+            case 4:
+                cout<<"MINIMUM DATA VALUE FIND IN THE TREE : "<<b.min()<<endl;
+                break;
+
+            case 5:
+                b.mirror(b.root);
+                cout<<"TREE AFTER MIRROR : ";
+                b.display_inorder(b.root);
+                b.mirror(b.root);
+                break;
+
+            case 6:
+                cout<<"ENTER THE VALUE TO SEARCH :";
+                int val;
+                cin>>val;
+                b.search(val);
+                break;
+
+            case 0:
+                break;
+      }
+    }while (ch != 0);
+  }
