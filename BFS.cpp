e71f9bbbac440a5d2c7e892b367e24aedb7afb93
Aryan N. Jain <aryanj1084@gmail.com>
2024-04-22 21:02:04 +0530

    BFS.cpp

--- BFS.cpp
+++ BFS.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 using namespace std;
 
-void BFS(int A[][6], int n) {
-    cout << "Enter Starting Vertex:"<<endl;
+const int MAX_SIZE = 10; // Maximum size for the matrix
+
+void BFS(int A[][MAX_SIZE], int n) {
+    cout << "Enter Starting Vertex:" << endl;
     int vtx;
-    cin>>vtx;
+    cin >> vtx;
     int q[20]{0};
-    int visited[7]{0}; 
+    int visited[MAX_SIZE]{0}; 
     int f = 0, r = -1; 
     q[++r] = vtx;
     visited[vtx] = 1; 
@@ -23,14 +25,28 @@ void BFS(int A[][6], int n) {
     }
     cout << endl;
 }
+
 int main() {
-    int A[6][6] = {{0,1,1,1,1,0},
-                   {1,0,0,1,0,0},
-                   {1,0,0,1,1,1},
-                   {1,1,1,0,0,1},
-                   {1,0,1,0,0,0},
-                   {0,0,1,1,0,0}};
-    BFS(A, 6);
+    int n;
+    cout << "Enter the size of the matrix: ";
+    cin >> n;
+
+    if (n > MAX_SIZE) {
+        cout << "Matrix size exceeds maximum limit." << endl;
+        return 1; // Exit with error
+    }
+
+    int A[MAX_SIZE][MAX_SIZE];
+
+    // Input adjacency matrix from the user
+    cout << "Enter the adjacency matrix:" << endl;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cin >> A[i][j];
+        }
+    }
+
+    BFS(A, n);
 
     return 0;
 }
